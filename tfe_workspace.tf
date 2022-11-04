78448b49ac2a1a37076c54d46aa57461b516295b
DaShaun <dashaun@dashaun.com>
2022-11-04 18:33:42 -0500

    no repo for default

--- tfe_workspace.tf
+++ tfe_workspace.tf
@@ -3,12 +3,6 @@ resource "tfe_workspace" "default" {
   name         = replace(each.key, ".", "-")
   organization = data.tfe_organization.default.name
   auto_apply   = false
-  vcs_repo {
-    identifier         = format("%s/%s", data.tfe_organization.default.name, each.key)
-    ingress_submodules = true
-    oauth_token_id     = data.tfe_oauth_client.github.oauth_token_id
-    branch             = "main"
-  }
 }
 
 resource "tfe_workspace" "auto_apply" {
